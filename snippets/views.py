d2057c2054b047c1cc7ff9f2aba5c672aa4625f5
Masashi Morita <masashi.morita@opentone.co.jp>
2019-12-20 10:13:49 +0900

    :boom: Use authenticated user's id as the owner id when creating or updating Snippets and operate only the current authenticated user's snippet

--- snippets/views.py
+++ snippets/views.py
@@ -1,5 +1,6 @@
 from rest_framework import generics
-from rest_framework import permissions
+from rest_framework import permissions, status
+from rest_framework.response import Response
 from snippets.models import Snippet
 from django.contrib.auth.models import User
 from snippets.serializers import SnippetSerializer, UserSerializer
@@ -9,10 +10,68 @@ class SnippetList(generics.ListCreateAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
 
+    def create(self, request, *args, **kwargs):
+        data = request.data.dict()
+        data.update({'owner_id': request.user.id})
+        serializer = self.get_serializer(data=data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+
+    def list(self, request, *args, **kwargs):
+        queryset = self.filter_queryset(self.get_queryset())
+        queryset = queryset.filter(
+            owner_id=request.user.id
+        )
+
+        page = self.paginate_queryset(queryset)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
+
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+
+    def get(self, request, *args, **kwargs):
+        if self.valid_owner(request):
+            return self.retrieve(request, *args, **kwargs)
+        return Response(status=status.HTTP_400_BAD_REQUEST)
+
+    def put(self, request, *args, **kwargs):
+        if self.valid_owner(request):
+            return self.update(request, *args, **kwargs)
+        return Response(status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, *args, **kwargs):
+        if self.valid_owner(request):
+            return self.delete(request, *args, **kwargs)
+        return Response(status=status.HTTP_400_BAD_REQUEST)
+
+    def update(self, request, *args, **kwargs):
+        partial = kwargs.pop('partial', False)
+        instance = self.get_object()
+        data = request.data.dict()
+        data.update({'owner_id': request.user.id})
+        serializer = self.get_serializer(instance, data=data, partial=partial)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+
+        if getattr(instance, '_prefetched_objects_cache', None):
+            # If 'prefetch_related' has been applied to a queryset, we need to
+            # forcibly invalidate the prefetch cache on the instance.
+            instance._prefetched_objects_cache = {}
+
+        return Response(serializer.data)
+
+    def valid_owner(self, request):
+        snippet = self.get_object()
+        return request.user.id == snippet.owner_id
 
 class UserList(generics.ListCreateAPIView):
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
